14dc362d088e63a87932c0fe43e8f4e252f2e564
Est-1y <terryiana22@gmail.com>
2023-10-03 05:33:12 -0700

    alphy

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
-    if chr(letter) != 'q' and chr(letter) is not 'e':
-        print("abcdefghijklmnopqrstuvwxyz"[::-1].replace('q', '').replace('e', ''))
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")

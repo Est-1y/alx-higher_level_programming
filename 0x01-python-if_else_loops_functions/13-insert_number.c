36f27b5d2850fa194582fba49ab906e6cf9afed9
Est-1y <terryiana22@gmail.com>
2023-10-03 07:16:05 -0700

    insert number

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * insert_node -inserts numbers into sorted linked list
+ * @head: points to head of the linked list
+ * @number: no. to be inserted
+ * Return: NULL (fail), pointer to the node (otherwise)
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *node = *head, *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
+	{
+		new->next = node;
+		*head = new;
+		return (new);
+	}
+
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
+}
--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -5,19 +5,19 @@
 
 /**
  * struct listint_s - linked lists
- * @m: the integer
+ * @n: the integer
  * @next: points next node
  * Description: linked list node structures
  *
  */
 typedef struct listint_s
 {
-	int m;
+	int n;
 	struct listint_s *next;
 } listint_t;
 
 size_t print_listint(const listint_t *h);
-listint_t *add_nodeint_end(listint_t **head, const int m);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 

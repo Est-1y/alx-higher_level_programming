f32f2177c92269ba252e3795bf2ea0985ffdb612
Est-1y <terryiana22@gmail.com>
2023-10-05 02:30:18 -0700

    hidden

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 
-names = dir(hidden_4)
-for name in sorted(names):
-  if not name.startswith("__"):
-    print(name)
 if __name__ == "__main__":
-  main()
+    """Print all names defined by hidden_4 module."""
+    import hidden_4
+
+    names = dir(hidden_4)
+    for name in names:
+        if name[:2] != "__":
+            print(name)

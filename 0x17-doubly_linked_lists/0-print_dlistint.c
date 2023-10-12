8a156aa097200a71f20b6c613247452a707ee38c
Est-1y <terryiana22@gmail.com>
2023-10-12 09:20:53 -0700

    dlistint

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+#include "lists.h"
+
+/**
+ * print_dlistint - prints dlistint elements
+ * @h: list head
+ * Return: nodes number
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+		return (count);
+
+	while (h->prev != NULL)
+		h = h->prev;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}

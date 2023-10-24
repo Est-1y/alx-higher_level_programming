cdffe8b8991c8301f8fb22c35a20b7c4d0471fad
Est-1y <133119207+Est-1y@users.noreply.github.com>
2023-10-24 20:18:47 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
 """Define a class Square."""
+
 
 class Square:
     """Represent a square."""
@@ -14,3 +16,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)

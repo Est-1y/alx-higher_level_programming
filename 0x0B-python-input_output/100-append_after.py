c3c361b36b5f223d91bf2bea9dbc8d15b43fe6ec
Est-1y <terryiana22@gmail.com>
2023-11-07 10:48:57 -0800

    append after

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+the append after function
+"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """appends "new_string" after a line containing"""
+    with open(filename, 'r', encoding='utf-8') as f:
+        line_list = []
+        while True:
+            line = f.readline()
+            if line == "":
+                break
+            line_list.append(line)
+            if search_string in line:
+                line_list.append(new_string)
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.writelines(line_list)

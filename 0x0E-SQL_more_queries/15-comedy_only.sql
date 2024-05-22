171a526a3c2e3d60d1b450d5d0b2772a5cd7140f
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-05-22 13:17:41 +0300

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,5 +1,5 @@
 -- Import the database dump from hbtn_0d_tvshows
-a script listing all Comedy shows in the database hbtn_0d_tvshows
+-- a script listing all Comedy shows in the database hbtn_0d_tvshows
 SELECT tv_shows.title
     FROM tv_shows
 INNER JOIN tv_show_genres

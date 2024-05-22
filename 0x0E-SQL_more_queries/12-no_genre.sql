517fb281f0b09307facaada89359d9fc2ce11c6f
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-05-22 13:06:54 +0300

    Create 12-no_genre.sql

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- same as 11-genre_id_all_shows.sql
+-- a script listing all shows contained in hbtn_0d_tvshows without a genre linked.
+SELECT tv_shows.title, tv_show_genres.genre_id
+    FROM tv_shows
+LEFT JOIN tv_show_genres
+    ON tv_shows.id = tv_show_genres.show_id
+    WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;

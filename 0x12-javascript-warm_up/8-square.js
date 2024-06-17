e9a594656545478ad0c4d74683591b65e4688b1e
Est-1y <terryiana22@gmail.com>
2024-06-17 11:08:28 +0000

    square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -10,7 +10,7 @@ if (isNaN(parseInt(size))) {
   for (let i = 0; i < squareSize; i++) {
     let row = '';
     for (let j = 0; j < squareSize; j++) {
-      row += 'X ';
+      row += 'X';
     }
     console.log(row);
   }

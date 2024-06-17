f7f51635049838035e863b81f87bb430c7331672
Est-1y <terryiana22@gmail.com>
2024-06-17 10:44:22 +0000

    multi c

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const x = parseInt(process.argv[2]);
 
+const x = process.argv[2];
 if (isNaN(x)) {
-    console.log('Missing number of occurrences');
+  console.log('Missing number of occurences');
 } else {
-    for (let i = 0; i < x; i++) {
-        console.log('C is fun');
-    }
+  for (let i = 0; i < parseInt(x); i++) {
+    console.log('C is fun');
+  }
 }

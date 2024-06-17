19463603b8432789cb396dec77d6056ef0e60a2d
Est-1y <terryiana22@gmail.com>
2024-06-17 09:34:37 +0000

    Languages

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+const message1 = 'C is fun';
+const message2 = 'Python is cool';
+const message3 = 'JavaScript is amazing';
+
+console.log(message1);
+console.log(message2);
+console.log(message3);

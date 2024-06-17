50b09ddd4b0ab7c3f7825fa462cb3b1bfa27492d
Est-1y <terryiana22@gmail.com>
2024-06-17 09:46:46 +0000

    Arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const argsCount = process.argv.length - 2;
+const argsNumber = process.argv.length - 2;
 
 if (argsCount === 0) {
-  console.log("No argument");
+  console.log('No argument');
 } else if (argsCount === 1) {
-  console.log("Argument found");
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }

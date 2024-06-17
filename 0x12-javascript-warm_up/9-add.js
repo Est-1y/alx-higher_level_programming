9923c7ae93d417e44eb37419d36c17131b02603f
Est-1y <terryiana22@gmail.com>
2024-06-17 11:57:44 +0000

    add

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,4 +1,12 @@
 #!/usr/bin/node
-function add(a, b) {
-  console.log(a + b);
+
+const Arg1 = parseInt(process.argv[2]);
+const Arg2 = parseInt(process.argv[3]);
+
+function add (a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    return NaN;
+  }
+  return a + b;
 }
+console.log(add(Arg1, Arg2));

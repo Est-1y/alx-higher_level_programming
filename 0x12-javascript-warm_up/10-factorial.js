976aea80800cfa9a7440b27efd3ca4d376db5031
Est-1y <terryiana22@gmail.com>
2024-06-17 12:25:25 +0000

    factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,14 +1,9 @@
 #!/usr/bin/node
 const n = parseInt(process.argv[2], 10);
-function factorial(n) {
-  if (isNaN(n)) || n === 0 {
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
     return 1;
   }
-
-  if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
-  }
+  return n * factorial(n - 1);
 }
 console.log(factorial(n));

ee85f2e6ba99bf70708435b8f9cfa6763bbf5efa
Est-1y <terryiana22@gmail.com>
2024-06-18 12:30:33 +0000

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const { dict } = require('./101-data');
-
-const occurrencesDict = {};
 //iterate throug the occurences
+const occurrencesDict = {};
 for (const userId in dict) {
   const occurrences = dict[userId];
 

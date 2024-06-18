960255e79eb916c647fa8b83305038aa16d5a79e
Est-1y <terryiana22@gmail.com>
2024-06-18 10:32:51 +0000

    Defining rectangle

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -3,3 +3,8 @@ class Rectangle:
     def __init__(self, w, h):
         self.width = w
         self.height = h
+
+# Usage
+my_rectangle = Rectangle(5, 10)
+print(my_rectangle.width)
+print(my_rectangle.height)

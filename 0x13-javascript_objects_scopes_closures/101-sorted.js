de24ca7eb9f5034fca9ab5ce671eed930aaaad2b
Est-1y <terryiana22@gmail.com>
2024-06-18 12:49:42 +0000

    Sorted occurences

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,15 +1,18 @@
 #!/usr/bin/node
+
 const { dict } = require('./101-data');
 
-const occurrencesDict = {};
+// Initializing a new object
+let sortedDict = {};
+
 for (const userId in dict) {
   const occurrences = dict[userId];
 
-  if (occurrencesDict.hasOwnProperty(occurrences)) {
-    occurrencesDict[occurrences].push(userId);
-  } else {
-    occurrencesDict[occurrences] = [userId];
+  if (!sortedDict[occurrences]) {
+    sortedDict[occurrences] = [];
   }
+
+  sortedDict[occurrences].push(userId);
 }
 
-console.log(occurrencesDict);
+console.log(sortedDict);

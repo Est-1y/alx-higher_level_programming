5cf698c02149274a0b28bd34341d2cc005a09a72
Est-1y <terryiana22@gmail.com>
2024-07-05 14:33:18 +0000

    search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+post to request http://0.0.0.0:5000/search_user..
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+
+    url = "http://0.0.0.0:5000/search_user"
+    payload = {"q": q}
+
+    response = requests.post(url, data=payload)
+
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data['id'], data['name']))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")

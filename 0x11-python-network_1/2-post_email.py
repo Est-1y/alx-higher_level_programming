0f6d01558510999432e273fb00dddfc0cee80070
Est-1y <terryiana22@gmail.com>
2024-07-05 13:54:40 +0000

    posting an email

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+display body
+"""
+
+import sys
+import urllib.parse
+import urllib.request
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    data = urllib.parse.urlencode({'email': email})
+    data = data.encode('utf-8')
+
+    req = urllib.request.Request(url, data=data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf-8'))

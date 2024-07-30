2696af58c0fb13e4e799cc2a1f658d415a4158a8
Est-1y <terryiana22@gmail.com>
2024-07-30 15:45:07 +0000

    writing a string to a file

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,10 +2,11 @@
 const fs = require('fs');
 
 const file = process.argv[2];
+
 const content = process.argv[3];
 
-fs.writeFile(file, content, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
+fs.writeFile(file, content, 'utf-8', (error) => {
+  if (error) {
+    console.error(error);
   }
 });

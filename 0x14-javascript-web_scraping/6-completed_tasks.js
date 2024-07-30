b69281f8db018d1f0e81610fb40b91a13a384c28
Est-1y <terryiana22@gmail.com>
2024-07-30 15:07:19 +0000

    completed task count

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+
+//request require module
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, function (error, response, body) {
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const completed = {};
+    const tasks = JSON.parse(body);
+    for (const i in tasks) {
+      const task = tasks[i];
+      if (task.completed === true) {
+        if (completed[task.userId] === undefined) {
+          completed[task.userId] = 1;
+        } else {
+          completed[task.userId]++;
+        }
+      }
+    }
+    console.log(completed);
+  } else {
+    console.log('An error occured. Status code: ' + response.statusCode);
+  }
+});

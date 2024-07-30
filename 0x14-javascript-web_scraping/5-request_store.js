02cda915eff54c6c7356fc911845d63b53d9a283
Est-1y <terryiana22@gmail.com>
2024-07-30 15:00:01 +0000

    request store

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,7 +3,7 @@
 const request = require('request');
 const fs = require('fs');
 
-request(process.argv[2], (error, response, body) {
+request(process.argv[2], function (error, response, body) {
   if (error == null) {
     fs.writeFileSync(process.argv[3], body);
   }

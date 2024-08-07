b105fdc4de069c4a19dbeaa462c1455c0312210d
Est-1y <133119207+Est-1y@users.noreply.github.com>
2024-08-07 15:38:25 +0300

    Create 8-script.js

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,10 @@
+const $ = window.$;
+$(document).ready(function () {
+  $.get(
+    'https://swapi-api.alx-tools.com/api/films/?format=json',
+    function (data) {
+      $.each(data.results, function (index, movie) {
+        $('UL#list_movies').append('<li>' + movie.title + '</li>');
+      });
+    });
+});
